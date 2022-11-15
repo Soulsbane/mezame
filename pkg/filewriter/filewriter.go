34230bb117e8ee286feaeed24b1339ab63ab382f
Paul Crane <paul@acheronsoft.com>
2022-11-14 18:06:15 -0600

    Initial setup of FileWriter type

--- pkg/filewriter/filewriter.go
+++ pkg/filewriter/filewriter.go
@@ -1,5 +1,18 @@
 package filewriter
 
+import "github.com/Soulsbane/mezame/pkg/drivepaths"
+
 const (
 	DefaultMezameFileName = ".mezame"
 )
+
+type FileWriter struct {
+	paths *drivepaths.DrivePaths
+}
+
+func New() *FileWriter {
+	var writer FileWriter
+	writer.paths = drivepaths.New()
+
+	return &writer
+}
