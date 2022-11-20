d3f040908dc7169bb41353f8773ef012eee53dbc
Paul Crane <paul@acheronsoft.com>
2022-11-19 19:59:36 -0600

    Use Go's map instead of go18ds.Map
    This breaks Save and Load temperarily which will be fixed in next commit.

--- pkg/drivepaths/drivepaths.go
+++ pkg/drivepaths/drivepaths.go
@@ -2,79 +2,74 @@ package drivepaths
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/daichi-m/go18ds/maps/hashmap"
 )
 
 type DrivePaths struct {
-	paths *hashmap.Map[string, bool]
+	paths map[string]bool
 }
 
 func New() *DrivePaths {
 	var locs DrivePaths
 
-	locs.paths = hashmap.New[string, bool]()
+	locs.paths = make(map[string]bool)
 	return &locs
 }
 
 func (d *DrivePaths) Load(fileName string) {
-	json, err := os.ReadFile(fileName)
+	// json, err := os.ReadFile(fileName)
 
-	if err != nil {
-		fmt.Println("Error loading drive paths: ", err)
-	}
+	// if err != nil {
+	// 	fmt.Println("Error loading drive paths: ", err)
+	// }
 
-	d.paths.FromJSON(json)
+	// d.paths.FromJSON(json)
 }
 
 func (d *DrivePaths) Save(fileName string) {
-	json, err := d.paths.ToJSON()
+	// json, err := d.paths.ToJSON()
 
-	if err != nil {
-		fmt.Println("Error processing drive paths: ", err)
-	}
+	// if err != nil {
+	// 	fmt.Println("Error processing drive paths: ", err)
+	// }
 
-	err = os.WriteFile(fileName, json, 0644)
+	// err = os.WriteFile(fileName, json, 0644)
 
-	if err != nil {
-		fmt.Println("Error loading drive paths: ", err)
-	}
+	// if err != nil {
+	// 	fmt.Println("Error loading drive paths: ", err)
+	// }
 }
 
 func (d *DrivePaths) Add(path string) {
-	d.paths.Put(path, true)
+	d.paths[path] = true
 }
 
 func (d *DrivePaths) Remove(path string) {
-	d.paths.Remove(path)
+	delete(d.paths, path)
 }
 
 func (d *DrivePaths) Enable(path string) {
-	d.paths.Put(path, true)
+	d.paths[path] = true
 }
 
 func (d *DrivePaths) Disable(path string) {
-	d.paths.Put(path, false)
+	d.paths[path] = false
 }
 
 func (d *DrivePaths) IsEnabled(path string) bool {
-	value, found := d.paths.Get(path)
-
-	if found {
-		return value
+	if _, ok := d.paths[path]; ok {
+		return d.paths[path]
+	} else {
+		return false
 	}
 
-	return found // True if the key was found false otherwise. The value variable will be nil.
 }
 
 func (d *DrivePaths) NumPaths() int {
-	return d.paths.Size()
+	return len(d.paths)
 }
 
 func (d *DrivePaths) Dump() {
-	for _, key := range d.paths.Keys() {
-		value, _ := d.paths.Get(key)
-		fmt.Println(key, value)
+	for path, enabled := range d.paths {
+		fmt.Printf("%s => %t\n", path, enabled)
 	}
 }
